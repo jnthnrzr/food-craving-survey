1c5f072c5765591bedb1fae310fb298e62a26eab
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-04-20 23:20:48 -0400

    Add TestConfig.

--- backend/application/config.py
+++ backend/application/config.py
@@ -6,9 +6,17 @@ load_dotenv(os.path.join(basedir, '.venv'))
 
 
 class Config:
+    DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_TYPE = 'filesystem'
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     CSRF_ENABLED = os.environ.get("CSRF_ENABLED")
 
     tracking = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False or tracking
+    SQLALCHEMY_TRACK_MODIFICATIONS = tracking
+
+
+class TestingConfig(Config):
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+    SQLALCHEMY_DATABASE_URI = "sqlite://"
