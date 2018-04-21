9b749781deaa0e454d7fa64a24f92ff6d319223d
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-04-20 23:21:13 -0400

    Add flask session to app init.

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_migrate import Migrate
+from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
 
 from application.config import Config
@@ -9,6 +10,7 @@ app.config.from_object(Config)
 
 db = SQLAlchemy(app=app)
 migrate = Migrate(app=app, db=db)
+sess = Session(app=app)
 
 from application import models
 from application import views
