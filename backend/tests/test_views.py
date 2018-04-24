306c462606060b6ce5fef710f7c74ee78a76bd5e
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-04-23 20:18:40 -0400

    Add setUp and tearDown for TestViews.

--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -1,19 +1,22 @@
 import unittest
 
-from application import app
+from application import app, db
+from application.config import TestingConfig
+from application.forms import InputForm
 
 
 class TestViews(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
+        """Set testing configuration before each test."""
+        app.config.from_object(TestingConfig)
+        self.app = app
+        self.client = app.test_client()
+        db.create_all()
 
     def tearDown(self):
-        pass
-
-    def test_index(self):
-        result = self.app.get('/')
-        self.assertEqual(result.data, b"Hello World!")
+        """Clear testing configuration after each test."""
+        db.session.remove()
+        db.drop_all()
 
 
 if __name__ == '__main__':
