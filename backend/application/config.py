f6d5807a8f36d935609269d70ff79e62c9eaad27
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2018-04-25 21:07:24 -0400

    Limit session file threshold to 25.

--- backend/application/config.py
+++ backend/application/config.py
@@ -6,12 +6,13 @@ load_dotenv(os.path.join(basedir, '.venv'))
 
 
 class Config:
+    CSRF_ENABLED = os.environ.get("CSRF_ENABLED")
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SESSION_FILE_THRESHOLD = 25
     SESSION_TYPE = 'filesystem'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    CSRF_ENABLED = os.environ.get("CSRF_ENABLED")
 
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     tracking = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
     SQLALCHEMY_TRACK_MODIFICATIONS = tracking
 
